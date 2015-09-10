6b57efd7a26e41af7276e8ba5875ac8a4fd486cf
Gregory Haskins <gregory.haskins@gmail.com>
2015-09-10 15:39:19 -0400

    Checkpoint
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- cluster.go
+++ cluster.go
@@ -12,9 +12,9 @@ import (
 )
 
 func main() {
-	id         := flag.String("id", "localhost:2001", "our identity")
+	id := flag.String("id", "localhost:2001", "our identity")
 	privateKey := flag.String("key", "key1.pem", "the path to our private key")
-	certsPath  := flag.String("certs", "certs", "the path to our membership definition")
+	certsPath := flag.String("certs", "certs", "the path to our membership definition")
 
 	flag.Parse();
 	fmt.Printf("id: %s, privatekey: %s, config: %s\n", *id, *privateKey, *certsPath)
@@ -30,7 +30,7 @@ func main() {
 		var block *pem.Block
 
 		block, remain = pem.Decode(remain)
-		if block == nil || block.Type != "CERTIFICATE"{
+		if block == nil || block.Type != "CERTIFICATE" {
 			break
 		}
 
@@ -47,7 +47,10 @@ func main() {
 		certs = append(certs, cert)
 	}
 
-	peers := map[string]*Identity{}
+	peers       := map[string]*Identity{}
+	clientPeers := map[string]*Identity{}
+	serverPeers := map[string]*Identity{}
+
 	var self *Identity
 
 	for _, cert := range certs {
@@ -59,7 +62,19 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Using %s with peers %v\n", self.Cert.Subject.CommonName, peers)
+	fmt.Printf("Using %s with peers:\n" , self.Cert.Subject.CommonName)
+
+	for _, peer := range peers {
+		fmt.Printf("\t%s - %s (", peer.Cert.Subject.CommonName, peer.Id)
+		if peer.Id < self.Id {
+			serverPeers[peer.Id] = peer
+			fmt.Printf("S")
+		} else {
+			clientPeers[peer.Id] = peer
+			fmt.Printf("C")
+		}
+		fmt.Printf(")\n")
+	}
 
 	var tlsCert *tls.Certificate
 	tlsCert, err = CreateTlsIdentity(self.Cert, *privateKey)
@@ -69,54 +84,56 @@ func main() {
 
 	connectionEvents := make(chan *Connection)
 
-	// First start our primary listener
-	go func() {
-		listener, err := Listen(tlsCert, self.Cert.Subject.CommonName)
-		if err != nil {
-			panic(err)
-		}
-
-		for {
-			var conn *Connection
-			var err error
-
-			conn, err = Accept(listener)
+	// First start our primary listener if we have at least one server
+	if len(serverPeers) > 0 {
+		go func() {
+			listener, err := Listen(tlsCert, self.Cert.Subject.CommonName)
 			if err != nil {
 				panic(err)
 			}
 
-			// Check to see if the connection is related to a peer we expect
-			if _, ok := peers[conn.Id.Id]; ok {
-				connectionEvents <- conn
-			} else {
-				log.Printf("Dropping unknown peer %v", conn.Id)
-			}
-		}
-
-	}()
-
-	// Now initiate a parallel workload to form connections with any of our peers
-	// that have an ID greater than our own
-	for _, peer := range peers {
-		if peer.Id > self.Id {
-			go func() {
-
+			for {
 				var conn *Connection
+				var err error
 
-				for {
-					var err error
-					conn, err = Dial(tlsCert, peer)
-					if err == nil {
-						continue
-					}
-					time.Sleep(time.Duration(5)*time.Second)
+				conn, err = Accept(listener)
+				if err != nil {
+					panic(err)
 				}
 
-				connectionEvents <- conn
-			}()
-		}
+				// Check to see if the connection is related to a peer we expect to be connecting
+				// to us as a client
+				if _, ok := serverPeers[conn.Id.Id]; ok {
+					connectionEvents <- conn
+				} else {
+					log.Printf("Dropping unknown peer %v", conn.Id)
+				}
+			}
+
+		}()
 	}
 
+	// Now initiate a parallel workload to form connections with any of our peers
+	// that we are a client of
+	for _, peer := range clientPeers {
+		go func() {
+
+			var conn *Connection
+
+			for {
+				var err error
+				conn, err = Dial(tlsCert, peer)
+				if err == nil {
+					continue
+				}
+				time.Sleep(time.Duration(5)*time.Second)
+			}
+
+			connectionEvents <- conn
+		}()
+	}
+
+	// Finally, wait for connections to come in
 	for {
 		conn := <-connectionEvents
 		fmt.Printf("new connection from %s", conn.Id.Id)
