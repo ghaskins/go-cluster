775445b1d0c8bbbce3b03a6092d4b4283c01f4f2
Gregory Haskins <gregory.haskins@gmail.com>
2015-11-11 21:54:02 -0500

    Change Quorum calculation to be just north of 50% so that we dont end up splitting down the middle for even numbers of members
    Signed-off-by: Gregory Haskins <gregory.haskins@gmail.com>

--- quorum.go
+++ quorum.go
@@ -5,5 +5,5 @@ import (
 )
 
 func ComputeQuorumThreshold(memberCount int) int {
-	return int(math.Ceil(float64(memberCount) / 2.0))
+	return int(math.Ceil(float64(memberCount) * .5000000001)) // something *just* north of 50%
 }
